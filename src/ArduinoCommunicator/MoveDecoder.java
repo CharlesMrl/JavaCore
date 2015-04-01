88690f5f374ecf9abd28c7146f27c644fed3602c
Bastien <bpascard@hotmail.com>
2015-04-01 11:13:52 +0200

    pieceEvent

--- src/ArduinoCommunicator/MoveDecoder.java
+++ src/ArduinoCommunicator/MoveDecoder.java
@@ -16,9 +16,9 @@ public class MoveDecoder {
         boolean isUp;
         
         public pieceEvent(String in){
-            if(in.charAt(0)!='u')isUp = false;
-            else if(in.charAt(0)!='d'){
-                isUp=true;
+            if(in.charAt(0)=='u')isUp = true;
+            else if(in.charAt(0)=='d'){
+                isUp=false;
             }
             pos=in.substring(1, 3);
         }
@@ -40,8 +40,10 @@ public class MoveDecoder {
         {
             System.out.println("roque !!");
             // Si 3 cases d'écart --> grand roque
-            if (((((mySplit[0].substring(1,3)).equals("a8"))&&((mySplit[1].substring(1,3)).equals("d8")))||(((mySplit[0].substring(1,3)).equals("a1"))&&((mySplit[1].substring(1,3)).equals("d1"))))
-                    || ((((mySplit[2].substring(1,3)).equals("a8"))&&((mySplit[3].substring(1,3)).equals("d8")))||(((mySplit[2].substring(1,3)).equals("a1"))&&((mySplit[3].substring(1,3)).equals("d1")))))
+            if (((((mySplit[0].substring(1,3)).equals("a8"))&&((mySplit[1].substring(1,3)).equals("d8")))
+                    ||(((mySplit[0].substring(1,3)).equals("a1"))&&((mySplit[1].substring(1,3)).equals("d1"))))
+                    || ((((mySplit[2].substring(1,3)).equals("a8"))&&((mySplit[3].substring(1,3)).equals("d8")))
+                    ||(((mySplit[2].substring(1,3)).equals("a1"))&&((mySplit[3].substring(1,3)).equals("d1")))))
             {
                 System.out.println("Grand roque!!");
                 coup = "O-O-O";
@@ -54,20 +56,27 @@ public class MoveDecoder {
         }
         
         //En passant
-        else if ((mySplit.length == 3)&&((mySplit[0].charAt(0) == 'u')&&(mySplit[1].charAt(0) == 'd')&&(mySplit[2].charAt(0) == 'u')))
+        else if ((mySplit.length == 3)&&((mySplit[0].charAt(0) == 'u')
+                &&(mySplit[1].charAt(0) == 'd')
+                &&(mySplit[2].charAt(0) == 'u')))
         {
             System.out.println("en passant!!!");
             coup = mySplit[0].substring(1, 3) + mySplit[1].substring(1, 3);
         }
         
         //Deplacement normal
-        else if ((mySplit.length == 2)&&((mySplit[0].charAt(0) == 'u')&&(mySplit[1].charAt(0) == 'd')))
+        else if ((mySplit.length == 2)
+                &&((mySplit[0].charAt(0) == 'u')
+                &&(mySplit[1].charAt(0) == 'd')))
         {
             coup = mySplit[0].substring(1, 3) + mySplit[1].substring(1, 3);
         }
         
         //Capture
-        else if ((mySplit.length == 3)&&((mySplit[0].charAt(0) == 'u')&&(mySplit[1].charAt(0) == 'u')&&(mySplit[2].charAt(0) == 'd')))
+        else if ((mySplit.length == 3)
+                &&((mySplit[0].charAt(0) == 'u')
+                &&(mySplit[1].charAt(0) == 'u')
+                &&(mySplit[2].charAt(0) == 'd')))
         {
             coup = mySplit[0].substring(1, 3) + mySplit[2].substring(1, 3);
         }
