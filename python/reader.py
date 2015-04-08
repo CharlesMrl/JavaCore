67e1e58cb352927808db760ebc3c73e9fd4f5d9c
Louis Syoen <louis.syoen@openmailbox.org>
2015-04-08 04:10:10 +0200

    ACM0 -> ACM1 wut ?

--- python/reader.py
+++ python/reader.py
@@ -2,10 +2,10 @@
 # -*- coding: latin-1 -*-
 
 import serial,time 
-ser = serial.Serial('/dev/ttyACM0', 9600)
+ser = serial.Serial('/dev/ttyACM1', 9600)
 time.sleep(1)
 logfile = open("reader.log", "w")
 while 1 :
 	s=ser.readline()
 	print(s)
-	logfile.write("{}".s."  Time: ".time.time())
+	logfile.write("{}".s)
