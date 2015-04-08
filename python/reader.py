99cf771e7ff0fbea256ee93ca37faef76f4ed5ea
Louis Syoen <louis.syoen@openmailbox.org>
2015-04-08 06:04:05 +0200

    yop

--- python/reader.py
+++ python/reader.py
@@ -2,9 +2,12 @@
 # -*- coding: latin-1 -*-
 
 import serial,time 
-ser = serial.Serial('/dev/ttyACM1', 9600)
+ser = serial.Serial('/dev/ttyACM1', 9600, timeout=1)
+time.sleep(1)
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
 time.sleep(1)
 while 1 :
-	s=ser.readline()
+	#s=ser.readline()
+	s=sio.readline()
 	print(s)
 	print('\n')
