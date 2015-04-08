4c6932725850e5b406881d6c941d0a62d31e44b1
Louis Syoen <louis.syoen@openmailbox.org>
2015-04-08 05:37:56 +0200

    test

--- python/writer.py
+++ python/writer.py
@@ -7,9 +7,8 @@ ser = serial.Serial('/dev/ttyACM1', 9600)
 time.sleep(1)
 s = sys.stdin.readline().strip()
 
-while 1:
-	ser.write(s + '$')
-	logfile = open("writer.log", "a")
-	logfile.write(s)
-	logfile.close()
-	s = sys.stdin.readline().strip()
+#while 1:
+ser.write(s + '$')
+logfile = open("writer.log", "a")
+logfile.write(s)
+logfile.close()
